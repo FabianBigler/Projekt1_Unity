d31aaa9c4955a162447791fb9230df84f6270a0a
FabianBigler <fabigler@hotmail.com>
2015-09-30 15:07:30 +0200

    added main menu

---
+++ Assets/MainMenu.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+var quit = false;
+
+
+
+function OnMouseUp()
+{
+    if (quit)
+    {
+        Application.Quit();
+    } else {
+        Application.LoadLevel(1);
+    }
+}
+
+function Update () {
+    if (Input.GetKey(KeyCode.Escape))
+    {
+        Application.Quit();
+    }
+}
+
