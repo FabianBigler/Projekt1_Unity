d4bfa2966cff68a6ea75a637caea37813c005e2b
Manuel Schmutz <manuel.schmutz@gmail.com>
2015-11-20 16:13:52 +0100

    added EnterRoomTrigger Script

---
+++ Assets/EnterRoomTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterRoomTrigger : MonoBehaviour {
+    public AudioSource ambienteSound;
+
+	// Use this for initialization
+	void Start () {
+        ambienteSound = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        ambienteSound.Play();
+    }
+}
