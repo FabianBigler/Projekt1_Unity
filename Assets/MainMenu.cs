d82ec729cd8736cac54b43a73e3850fb2652c834
Marco Kilchhofer <marco@kilchhofer.info>
2015-11-20 16:54:35 +0100

    changed MainMenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+
+	// Use this for initialization
+	public void StartGame () {
+        Application.LoadLevel(startLevel);
+	}
+	
+	// Update is called once per frame
+	public void QuitGame () {
+        Application.Quit();
+	}
+}
