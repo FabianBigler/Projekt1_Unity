8c0a5b9a5d8b39c8339fc876dd3bc5a9752326fe
Marco Kilchhofer <marco@kilchhofer.info>
2015-11-20 17:38:23 +0100

    added 3D key asset on forest scene

---
+++ Assets/ItemRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotator : MonoBehaviour {
+    public double rotationsPerMinute = 10.0;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, (float)(6.0 * rotationsPerMinute * Time.deltaTime), 0);
+    }
+}
