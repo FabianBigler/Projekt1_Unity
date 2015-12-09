a39c2ce4d84cb7a7fe6a4441aed30fc72f295895
Manuel Schmutz <manuel.schmutz@gmail.com>
2015-12-09 17:18:28 +0100

    added content for BoxRiddle

---
+++ Assets/ObjectPushPhysics.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPushPhysics : MonoBehaviour
+{
+
+    public float pushForce = 2.0f;
+    public string gameTagCollider = "pushable";
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.tag == gameTagCollider)
+        {
+            Rigidbody body = hit.collider.attachedRigidbody;
+
+            //checking whether rigidbody is either non-existant or kinematic
+            if (body == null || body.isKinematic)
+                return;
+
+            if (hit.moveDirection.y < -.3f)
+                return;
+
+            //set up push direction for object
+            Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            //apply push force to object
+            body.velocity = pushForce * pushDirection;
+        }
+    }
+}
