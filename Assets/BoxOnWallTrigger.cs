bdc742f67f9e6ce45ce80e1a885a677adef3470d
Manuel Schmutz <manuel.schmutz@gmail.com>
2015-12-09 19:21:59 +0100

    implemented first version of BoxRiddle

---
+++ Assets/BoxOnWallTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoxOnWallTrigger : MonoBehaviour
+{
+    public GameObject KeyToActivate;
+
+    private bool successTriggered;
+
+    void Start()
+    {
+        successTriggered = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("element with name <<" + other.gameObject.name + ">> entered triggerCollider");
+        if (other.gameObject.name.Equals("targetBox"))
+        {
+            CheckSuccess();
+        }
+    }
+
+    private void CheckSuccess()
+    {
+            if (!successTriggered)
+            {
+                successTriggered = true;
+                Debug.Log("Mission completed!");
+                KeyToActivate.SetActive(true);
+            }
+        }
+
+}
